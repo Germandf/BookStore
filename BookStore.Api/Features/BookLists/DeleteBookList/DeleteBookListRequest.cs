e7f2ab3df5f62938ff9c1319043bcdef88f23831
Germán De Francesco <germandefrancesco@hotmail.com>
2024-01-27 16:35:17 -0300

    Add DeleteBookList feature

---
+++ BookStore.Api/Features/BookLists/DeleteBookList/DeleteBookListRequest.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace BookStore.Api.Features.BookLists.DeleteBookList;
+
+public record DeleteBookListRequest(
+    Guid Id)
+    : IRequest<Result<Success>>;
---
+++ BookStore.Api/Features/BookLists/DeleteBookList/DeleteBookListValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace BookStore.Api.Features.BookLists.DeleteBookList;
+
+public class DeleteBookListValidator : AbstractValidator<DeleteBookListRequest>
+{
+    public DeleteBookListValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+    }
+}
