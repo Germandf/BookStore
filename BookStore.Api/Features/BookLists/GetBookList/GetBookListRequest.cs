6a7c6628b65f3a864f4a88c967154615e4bdb6ce
Germán De Francesco <germandefrancesco@hotmail.com>
2024-01-27 16:46:50 -0300

    Add GetBookList feature

---
+++ BookStore.Api/Features/BookLists/GetBookList/GetBookListRequest.cs
@@ -0,0 +1,7 @@
+﻿using BookStore.Api.Features.BookLists;
+using FluentResults;
+using MediatR;
+
+namespace BookListStore.Api.Features.BookLists.GetBookList;
+
+public record GetBookListRequest(Guid Id) : IRequest<Result<BookList>>;
---
+++ BookStore.Api/Features/BookLists/GetBookList/GetBookListValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace BookListStore.Api.Features.BookLists.GetBookList;
+
+public class GetBookListValidator : AbstractValidator<GetBookListRequest>
+{
+    public GetBookListValidator()
+    {
+        RuleFor(request => request.Id).NotEmpty();
+    }
+}
